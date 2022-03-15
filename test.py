0a8794e6172241100f5d729446d6d947943c6a36
nemo <56830656+jiayulaw@users.noreply.github.com>
2022-03-15 22:07:56 +0800

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
+
+JsonFileName = '2021-08-15 16-18-18-x-babyy' + ".json"
+print(JsonFileName)
+# JsonFilePath = os.path.join(directory, JsonFileName)
+# JsonFilePath = rf"static/image uploads/end device 2/"+JsonFileName
+JsonFilePath = os.path.join(rf"static/image uploads/end device 2/", JsonFileName)
+print(JsonFilePath)
+JsonFilePath = os.path.join(BASE_DIR, JsonFilePath)
+
+print(JsonFilePath)
+# Record to database the new image    
+
+if os.path.exists(JsonFilePath):
+    print("yes")
+else:
+    print("no")
--- tests/test-SFTP-to-server.py
+++ tests/test-SFTP-to-server.py
@@ -19,8 +19,8 @@ path = '/var/www/Elephant_Web_App/static/image uploads/' + station
 # the file should have the format of '2021-10-17 22-30' at server side
 # image = r"C:\Users\user10\Desktop\2022-02-21 21-21-21-x-maggimee.jpg"
 
-image = r'C:\Users\user10\Desktop\Hobby\Programming\EEEY3 Project\Web App\Elephant_Web_App_v2\static\image uploads\end device 2\2021-09-15 16-18-18xxxsottlar_annotated.jpg'
-jsonn = r'C:\Users\user10\Desktop\Hobby\Programming\EEEY3 Project\Web App\Elephant_Web_App_v2\static\image uploads\end device 2\2021-09-15 16-18-18xxxsottlar_annotated.json'
+image = r'C:\Users\user10\Desktop\Hobby\Programming\EEEY3 Project\Web App\Elephant_Web_App_v2\static\image uploads\end device 2\2021-08-15 16-18-18-x-babyy.jpeg'
+jsonn = r'C:\Users\user10\Desktop\Hobby\Programming\EEEY3 Project\Web App\Elephant_Web_App_v2\static\image uploads\end device 2\2021-08-15 16-18-18-x-babyy.json'
 
 
 
