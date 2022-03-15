5c949baf42f6a0e5983693142b3a81d693be3f49
nemo <56830656+jiayulaw@users.noreply.github.com>
2022-03-15 17:32:14 +0800

    removed try except from update img function

--- tests/test-SFTP-to-server.py
+++ tests/test-SFTP-to-server.py
@@ -17,11 +17,17 @@ path = '/var/www/Elephant_Web_App/static/image uploads/' + station
 
 # this is the path to img
 # the file should have the format of '2021-10-17 22-30' at server side
-image = r"C:\Users\user10\Desktop\2022-02-21 21-21-21-x-maggimee.jpg"
+# image = r"C:\Users\user10\Desktop\2022-02-21 21-21-21-x-maggimee.jpg"
+
+image = r'C:\Users\user10\Desktop\Hobby\Programming\EEEY3 Project\Web App\Elephant_Web_App_v2\static\image uploads\end device 2\2021-07-15 16-18-18-x-elephant.jpg'
+jsonn = r'C:\Users\user10\Desktop\Hobby\Programming\EEEY3 Project\Web App\Elephant_Web_App_v2\static\image uploads\end device 2\2021-07-15 16-18-18-x-elephant.json'
+
 
 
 with pysftp.Connection(HOST_NAME, username=USERNAME, private_key=KEYFILE, cnopts=cnopts) as sftp:
     print("CONNECTED!!")
     with sftp.cd(f"{path}"):
+        sftp.put(jsonn)
         sftp.put(image)
+        
 
