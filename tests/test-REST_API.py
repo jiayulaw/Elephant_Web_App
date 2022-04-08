ae0b3838f3e81e1df1a4bf659c04842d3a9e5796
nemo <56830656+jiayulaw@users.noreply.github.com>
2022-04-08 15:21:51 +0800

    changed UI to using Bootstrap 5

--- tests/test-REST_API.py
+++ tests/test-REST_API.py
@@ -3,17 +3,17 @@
 
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+# BASE = "http://127.0.0.1:5000/"
 
-# BASE = "https://d3m318b1ejw1x6.cloudfront.net/"
+BASE = "https://d3m318b1ejw1x6.cloudfront.net/"
 # set device_id to specify the device to update
 # i.e., 1 is for end device 1, 2 is for end device 2, and 3 is for ...
 # set last_seen to modify time
 # set message to attach a message
 # the name will be what will be displayed on server
 # set status as online
-device_id = "3"
-name = "end device 3"
+device_id = "1"
+name = "end device 1"
 last_seen = "2022-04-06 20-46-20"
 message  = "Elephant sleeping!"
 status = "Online"
@@ -21,7 +21,7 @@ battery_voltage = "15"
 battery_current = "0.667"
 battery_temp_1 = "31"
 battery_temp_2 = "32"
-ambient_temp = "28"
+ambient_temp = "25"
 
 # this is a function to send 
 def update_device_stat(device_id, name, last_seen, message, status, battery_voltage, battery_current, battery_temp_1, battery_temp_2, ambient_temp):
