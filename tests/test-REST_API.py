5216c29b72f601fd4d33d4a379f5daf9a1ea9fbc
nemo <56830656+jiayulaw@users.noreply.github.com>
2022-04-09 01:15:29 +0800

    design of tables and overall

--- tests/test-REST_API.py
+++ tests/test-REST_API.py
@@ -3,9 +3,9 @@
 
 import requests
 
-# BASE = "http://127.0.0.1:5000/"
+BASE = "http://127.0.0.1:5000/"
 
-BASE = "https://d3m318b1ejw1x6.cloudfront.net/"
+# BASE = "https://d3m318b1ejw1x6.cloudfront.net/"
 # set device_id to specify the device to update
 # i.e., 1 is for end device 1, 2 is for end device 2, and 3 is for ...
 # set last_seen to modify time
